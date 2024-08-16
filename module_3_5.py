a4e3141bd589aa7e1c5d5a26c3ea561dc568e40a
igortadom <tadom@yandex.ru>
2024-08-16 06:29:52 +0300

    Add files via upload

---
+++ module_3_5.py
@@ -0,0 +1,11 @@
+def get_multiplied_digits(n):
+    str_number = str(n)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    elif len(str_number) < 2:
+        return first
+
+
+print(get_multiplied_digits(40203))
+
